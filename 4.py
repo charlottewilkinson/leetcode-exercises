c07d327f338c949177dbecf3a9b66c1ec0e6f973
u97032cw <charlotte.wilkinson@student.manchester.ac.uk>
2025-09-20 02:27:13 +0100

    Hard 4 runs but doesn't pass all testcases.

---
+++ 4.py
@@ -0,0 +1,44 @@
+from typing import List
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        n = len(nums1) + len(nums2)
+        
+        p1 = 0
+        p2 = 0
+        merged_list = []
+
+        while p1 + p2 <= (n // 2) + 1:
+            if p1 == len(nums1):
+                merged_list += nums2[p2: p2 + (n // 2) + 1 - len(merged_list)]
+                break
+            elif p2 == len(nums2):
+                merged_list += nums1[p1: p2 + (n // 2) + 1 - len(merged_list)]
+                break
+            elif nums1[p1] < nums2[p2]:
+                merged_list.append(nums1[p1])
+                p1 += 1
+            elif nums2[p2] < nums1[p1]:
+                merged_list.append(nums2[p2])
+                p2 += 1
+            else:
+                # nums1[p1] == nums2[p2]
+                merged_list.append(nums1[p1])
+                merged_list.append(nums2[p2])
+                p1 += 1
+                p2 += 1
+        
+        if n % 2 == 0:
+            # even
+            median = (merged_list[-2] + merged_list[-1]) / 2
+
+        else:
+            # odd
+            median = merged_list[-1]
+
+        return median
+
+
+
+s = Solution()
+print(s.findMedianSortedArrays(nums1 = [1,2], nums2 = [3, 4]))
